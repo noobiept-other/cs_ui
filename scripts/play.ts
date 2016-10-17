18691be847ade521153feef04fd6856fde7ddfab
Pedro Ferreira <plferreira4@gmail.com>
2016-10-17 22:03:30 +0100

    - add a timer that counts the time it has passed since we started searching for a game
    - add another timer to simulate the matchmaking, after 10 seconds it shows the accept dialog message

--- scripts/play.ts
+++ scripts/play.ts
@@ -5,6 +5,13 @@ module Play {
     var PLAYING = false;
     var ACCEPT_DIALOG: HTMLElement;
 
+    // time has passed during game search
+    var TIMER_ID: number | null = null;
+    var TIMER_COUNT = 0;    // in seconds
+
+    // time until the match is "found"
+    var MATCH_FOUND_ID: number | null = null;
+
 
     export function init() {
 
@@ -24,12 +31,18 @@ module Play {
 
     function startSearching() {
         showOverlay();
+        startTimer();
+        startMatchFoundTimer();
+
         PLAYING = true;
     }
 
 
     function stopSearching() {
         hideOverlay();
+        resetTimer();
+        resetMatchFoundTimer();
+
         PLAYING = false;
     }
 
@@ -49,12 +62,59 @@ module Play {
     }
 
 
-    export function showAcceptDialog() {
+    function showAcceptDialog() {
         ACCEPT_DIALOG.classList.remove( 'hidden' );
     }
 
 
     function hideAcceptDialog() {
         ACCEPT_DIALOG.classList.add( 'hidden' );
+    }
+
+
+    function startTimer() {
+        TIMER_COUNT = 0;
+
+        var elements = document.querySelectorAll( '.searchTime' );
+        updateTimer( elements );
+
+        TIMER_ID = window.setInterval( function () {
+            TIMER_COUNT++;
+            updateTimer( elements );
+        }, 1000 );
+    }
+
+
+    function updateTimer( elements: NodeListOf<Element> ) {
+        for ( var a = 0; a < elements.length; a++ ) {
+            elements[ a ].textContent = TIMER_COUNT + 's';
+        }
+    }
+
+
+    function resetTimer() {
+        if ( TIMER_ID !== null ) {
+            window.clearInterval( TIMER_ID );
+            TIMER_ID = null;
+        }
+    }
+
+
+    function startMatchFoundTimer() {
+        MATCH_FOUND_ID = window.setTimeout( function () {
+
+            showAcceptDialog();
+
+            stopSearching();
+            MATCH_FOUND_ID = null;
+        }, 10000 );
+    }
+
+
+    function resetMatchFoundTimer() {
+        if ( MATCH_FOUND_ID !== null ) {
+            window.clearTimeout( MATCH_FOUND_ID );
+            MATCH_FOUND_ID = null;
+        }
     }
 }
